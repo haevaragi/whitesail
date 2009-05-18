0d6721b7e8c7dc4c271b6e96c1ddae77bd08027d
baifanchn <baifanchn@94dd0156-0a65-11de-98bb-07cc9828235c>
2009-05-18 15:03:08 +0000

    change to git

--- trunk/whitesail/src/main/java/cn/edu/bit/whitesail/Cobra.java
+++ trunk/whitesail/src/main/java/cn/edu/bit/whitesail/Cobra.java
@@ -1,4 +1,5 @@
 package cn.edu.bit.whitesail;
+
 import org.lobobrowser.html.*;
 import org.lobobrowser.html.test.*;
 import org.lobobrowser.html.parser.*;
@@ -10,35 +11,35 @@ import org.w3c.dom.html2.*;
 import java.net.*;
 import java.io.*;
 
-
 public class Cobra {
-    private static final String TEST_URI = "http://localhost/test.html";
+
+    private static final String TEST_URI = "http://www.google.com";
 
     public static void main(String[] args) throws Exception {
         UserAgentContext uacontext = new SimpleUserAgentContext();
         //((SimpleUserAgentContext)uacontext).setExternalCSSEnabled(false);
-        ((SimpleUserAgentContext)uacontext).setScriptingEnabled(true);
+        ((SimpleUserAgentContext) uacontext).setScriptingEnabled(true);
         DocumentBuilderImpl builder = new DocumentBuilderImpl(uacontext);
         URL url = new URL(TEST_URI);
-      
+
         InputStream in = url.openConnection().getInputStream();
         int length = 0;
-        HTMLCollection images = null;
+        HTMLCollection link = null;
         try {
             Reader reader = new InputStreamReader(in, "ISO-8859-1");
             InputSourceImpl inputSource = new InputSourceImpl(reader, TEST_URI);
             Document d = builder.parse(inputSource);
             HTMLDocumentImpl document = (HTMLDocumentImpl) d;
-            
-            images = document.getLinks();
-            length = images.getLength();
-            
+
+            link = document.getLinks();
+            length = link.getLength();
+
         } finally {
             in.close();
         }
-         for(int i = 0; i < length; i++) {
-                System.out.println(i + ": " + images.item(i));
-            }
+        for (int i = 0; i < length; i++) {
+            System.out.println(i + ": " + link.item(i));
+        }
         System.out.println(length);
     }
 }
